b4dd9ea7424dffa53d398740b2bfc61b83eb4c4c
NathanAmu <165595361+NathanAmu@users.noreply.github.com>
2024-12-31 17:25:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Habilita o CORS para permitir requisições de outros domínios
 
 # Estoque inicial
 estoque = {"quantidade": 10}
